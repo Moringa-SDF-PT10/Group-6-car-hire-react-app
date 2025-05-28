f22816f111958619c716d3647915b404ec3cf7d6
Victor Kimathi <vkimathi97@gmail.com>
2025-05-28 10:31:33 +0300

    update json-server for backend deployment

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 // server.js
-import jsonServer from 'json-server';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Setup __dirname equivalent in ES Module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
