88346383b17ef2accdbb9bb63c42e8509559d52a
Moses Dianga <mosesdianga@gmail.com>
2025-05-31 13:30:55 +0300

    all changes in the context folder

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { AuthContext } from './AuthContext';
 
 export function AuthProvider({ children }) {
