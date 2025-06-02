0e8a8191d5592118233cf93d7f3cbce32631c241
Victor Kimathi <vkimathi97@gmail.com>
2025-06-02 14:43:28 +0300

    updatedjson-server to handle CORS

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-// server.js
 const jsonServer = require('json-server');
+const cors = require('cors');  // <-- Import cors
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3000;
 
+server.use(cors({
+  origin: 'http://localhost:5173',
+}));
+
 server.use(middlewares);
 server.use(router);
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
